302165ca86532a3b4519bdd9ac18585348b7f0e7
GBartkoski <gabrielbartkoski@gmail.com>
2026-09-12 14:14:48 -0700

    Add files via upload
    Initial code for website

---
+++ js-online-shop/app.js
@@ -0,0 +1,9 @@
+let productName = "NVIDIA GeForce RTX 3080";
+let price = 699.99;
+let inStock = true;
+let category = "Electronics";
+
+console.log("Product Name: " + productName);
+console.log("Price: $" + price);
+console.log("In Stock: " + inStock);
+console.log("Category: " + category);
